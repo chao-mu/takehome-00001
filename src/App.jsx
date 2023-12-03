1ef2bad3a3173a6c3c303934296c62ba42e19a0e
Danimal <danimal@hackpoetic.com>
2023-12-03 18:48:41 -0500

    choose field types dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import DynamicForm from "./components/form/DynamicForm.jsx";
 
+import payrollEnquiryForm from "./data/forms/payroll-enquiry.json";
+
 function App() {
   return (
     <main>
-      <DynamicForm />
+      <DynamicForm {...payrollEnquiryForm} />
     </main>
   );
 }
--- src/components/form/DynamicForm.jsx
+++ src/components/form/DynamicForm.jsx
@@ -1,23 +1,66 @@
-import Select from "./Select.jsx";
-import DateInput from "./DateInput.jsx";
-import TextArea from "./TextArea.jsx";
+// React
+import { lazy } from "react";
 
+// Community
+import PropTypes from "prop-types";
+
+// Ours - Stylesheets
 import styles from "./DynamicForm.module.css";
 
-function DynamicForm() {
+const componentLookup = {
+  select: lazy(() => import("./Select.jsx")),
+  date: lazy(() => import("./DateInput.jsx")),
+  textarea: lazy(() => import("./TextArea.jsx")),
+};
+
+// Only props of these names will be passed from meta to field component
+const allowedFieldProps = ["id", "options", "label"];
+
+function DynamicForm({ title, description, fields }) {
   return (
     <section className={styles.parent}>
       <hgroup>
-        <h1>Title</h1>
-        <p>subheading here</p>
+        <h1>{title}</h1>
+        <p role="doc-subtitle">{description}</p>
       </hgroup>
       <form>
-        <Select />
-        <DateInput />
-        <TextArea />
+        {fields.map((field) => {
+          const FieldComp = componentLookup[field.type];
+
+          const props = Object.fromEntries(
+            allowedFieldProps.map((prop) => [prop, field[prop]]),
+          );
+
+          return <FieldComp key={field.id} {...props} />;
+        })}
       </form>
     </section>
   );
 }
+
+const fieldShape = {
+  id: PropTypes.string.isRequired,
+  type: PropTypes.oneOf(["select", "date", "textarea"]).isRequired,
+  label: PropTypes.string.isRequired,
+  options: PropTypes.arrayOf(PropTypes.string), // For select type
+};
+
+const conditionShape = {
+  type: PropTypes.oneOf(["valueIn", "valueNotEmpty"]).isRequired,
+  fieldId: PropTypes.string.isRequired,
+  value: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+const ruleShape = {
+  if: PropTypes.shape(conditionShape).isRequired,
+  then: PropTypes.shape(conditionShape).isRequired,
+};
+
+DynamicForm.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  fields: PropTypes.arrayOf(PropTypes.shape(fieldShape)).isRequired,
+  rules: PropTypes.arrayOf(PropTypes.shape(ruleShape)).isRequired,
+};
 
 export default DynamicForm;
